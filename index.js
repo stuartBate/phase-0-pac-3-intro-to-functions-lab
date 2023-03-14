148aaf43ab0719eb20b02388a2d2a6c73578acd1
Stuart Bate <bate.stuart93@gmail.com>
2023-03-13 19:02:42 -0700

    functions lab

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+    return string.toUpperCase();
+}
+shout("hello");
+
+function whisper(string){
+    return string.toLowerCase();
+}
+whisper("HELLO");
+
+function logShout(string){
+    console.log(string.toUpperCase())
+}
+logShout("hello");
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+logWhisper("HELLO");
+
+function sayHiToHeadphonedRoommate(string){
+    if(string === string.toLowerCase()){
+        return `I can't hear you!`;
+    }else if(string === string.toUpperCase()){
+        return `YES INDEED!`;
+    } else if (string === `Let's have dinner together!`){
+        return "I would love to!";
+    }
+}
